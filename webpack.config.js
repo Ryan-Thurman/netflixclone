2be06148d35d5b7759b3583cc9ab31b161275621
Ryan Thurman <ryan.thur@gmail.com>
2016-05-21 17:35:31 -0700

    first

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  context: __dirname,
+  entry: './js/ClientApp.jsx',
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  }
+}
